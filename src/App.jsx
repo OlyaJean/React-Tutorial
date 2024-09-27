0dfd7ff37752e415e9f26fba5bf61a1d161f5fde
OlyaJean <olga_lavida@yahoo.com>
2024-09-27 19:26:06 -0400

    final steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Props from "./Props";
 import Props_with_Destructuring from "./Props_with_Destructuring";
 
@@ -9,11 +9,22 @@ const array = ['hello','bye'];
 let data = 'hello'
 array.push('wow');
 
+//------
 
 let [x,setX] = useState(0)
 const clickMe = () => {
-  setX(x + 1)
+  setX(x + 1)}
+
+//------
+
+const inputValue = useRef(null);
+//we created value state with an empty array. this this where we will store all our input value to display it as a list
+const [value,setValue] = useState([])
+const getInputValue = () => {
+  setValue([...value,inputValue.current.value])
 }
+
+
   return (
     <>
     {/*To display array as a list */}
@@ -26,6 +37,13 @@ const clickMe = () => {
    {/*We created Props.jsx to pass our x element to there using props. Name of the props will be count */}
    <Props count={x}/>
    <Props_with_Destructuring count={x}/>
+
+     {/*Now we will create input field, button and all input value will be displayed below as a list. We will use useRef hook. */}
+
+     <input ref={inputValue} type="text" />
+     <button onClick={getInputValue}>click me</button>
+     {value.map((val,index)=>{ return <p key={index}>{val}</p>})}
+
     </>
   )
 }
--- src/Props_with_Destructuring.jsx
+++ src/Props_with_Destructuring.jsx
@@ -5,6 +5,8 @@ const Props_with_Destructuring = ({count}) => {
     <div>
         {/* Here we passed props without additional words. just a prop name with curly braces */}
          {/* We can pass multiple props using comma inside the curly braces */}
+
+                {/* Or even a function in the prop and use that prop for any event listener */}
       <p>Another component for props example</p>
       <p>{count}</p>
     </div>
